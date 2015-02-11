1244eb78db21be8aa6dec93ff28daf55674644c5
Confile <michael.gorski82@googlemail.com>
2015-02-11 19:29:30 +0100

    getBuildNumber

--- www/appVersion.js
+++ www/appVersion.js
@@ -4,4 +4,8 @@ AppVersion.prototype.getVersionNumber = function(successCallback, errorCallback)
 	cordova.exec(successCallback, errorCallback, "AppVersion", "getVersionNumber", []);
 };
 
+AppVersion.prototype.getBuildNumber = function(successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, "AppVersion", "getBuildNumber", []);
+};
+
 module.exports = new AppVersion();
